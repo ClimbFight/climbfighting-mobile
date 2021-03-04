4d4ce4cae1fa852626e87273737bb9d5ffed757f
yujeongJeon <workingnewjeong@gmail.com>
2021-03-04 18:50:08 +0900

    [ClimbFight#1] 💄 styled-components test

--- app/screens/home/Diary.js
+++ app/screens/home/Diary.js
@@ -1,32 +1,30 @@
-
 import React from 'react';
-import { View, Text } from 'react-native';
-import { createBottomTabNavigator, BottomTabBar } from "react-navigation-tabs";
+import {View, Text} from 'react-native';
+import {createBottomTabNavigator, BottomTabBar} from 'react-navigation-tabs';
 
 import Home from './Home';
 import Search from './Search';
 import Write from './Write';
 import Alaram from './Alaram';
 import MyPage from './MyPage';
-import { createAppContainer } from 'react-navigation';
+import {createAppContainer} from 'react-navigation';
 
 const Diary = createBottomTabNavigator({
-        home: {
-            screen: Home
-        },
-        search: {
-            screen: Search
-        },
-        write: {
-            screen: Write
-        },
-        alaram: {
-            screen: Alaram
-        },
-        mypage: {
-            screen: MyPage
-        }
-    
-    });
+  home: {
+    screen: Home,
+  },
+  search: {
+    screen: Search,
+  },
+  write: {
+    screen: Write,
+  },
+  alaram: {
+    screen: Alaram,
+  },
+  mypage: {
+    screen: MyPage,
+  },
+});
 
 export default Diary;
--- app/screens/home/Home.js
+++ app/screens/home/Home.js
@@ -1,13 +1,19 @@
-
 import React from 'react';
-import { View, Text } from 'react-native';
+import {View, Text} from 'react-native';
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  color: #bbb;
+  margin: 1rem;
+`;
 
 const Home = () => {
-    return(
-        <View>
-            <Text>Home</Text>
-        </View>
-    );
-}
+  return (
+    <View>
+      <Title>This is Title</Title>
+      <Text>Home</Text>
+    </View>
+  );
+};
 
 export default Home;
