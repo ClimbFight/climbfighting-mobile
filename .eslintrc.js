ece054806fd0a8e7ba20f8aa1eea46401ee1cb1e
yujeongJeon <workingnewjeong@gmail.com>
2021-03-05 22:03:44 +0900

    [ClimbFight#1] 🍱 🐛 keystore setting, styled-component test

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,7 +17,7 @@ module.exports = {
         ecmaVersion: 12,
         sourceType: 'module',
     },
-    plugins: ['react', 'prettier'],
+    plugins: ['react', 'prettier', 'styled-components-a11y'],
     rules: {
         'react/jsx-filename-extension': [
             1,
--- app/screens/auth/Login.js
+++ app/screens/auth/Login.js
@@ -1,61 +1,64 @@
 import React from 'react'
-import {View, Text, StyleSheet, TouchableOpacity} from 'react-native'
+import {View, Text} from 'react-native'
+import styled from 'styled-components'
 
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-    },
+const Container = styled.View`
+    flex: 1;
+`
 
-    loginContainer: {
-        alignItems: 'center',
-        marginBottom: 50,
-    },
+const LoginContainer = styled.View`
+    align-items: center;
+    margin-bottom: 50px;
+`
 
-    loginButton: {
-        width: '80%',
-        height: '15%',
-        borderWidth: 1,
-        borderColor: 'black',
-        alignItems: 'center',
-        justifyContent: 'center',
-        margin: 10,
-    },
+const LoginButton = styled.TouchableOpacity`
+    width: 80%;
+    height: 15%;
+    border-width: 1;
+    border-color: black;
+    align-items: center;
+    justify-content: center;
+    margin: 10px;
+`
 
-    loginHelpContainer: {
-        flex: 1,
-        flexDirection: 'row',
-    },
-})
+const ExtraInfoButton = styled.TouchableOpacity`
+    width: 50%;
+    align-items: center;
+`
+
+const LoginHelpContainer = styled.View`
+    flex: 1;
+    flex-direction: row;
+`
 
 const Login = ({navigation}) => (
-    <View style={styles.container}>
+    <Container>
         <View style={{height: '50%'}} />
-        <View style={styles.loginContainer}>
-            <TouchableOpacity style={styles.loginButton}>
+        <LoginContainer>
+            <LoginButton>
                 <Text>Google 로그인</Text>
-            </TouchableOpacity>
+            </LoginButton>
 
-            <TouchableOpacity style={styles.loginButton}>
+            <LoginButton>
                 <Text>네이버 로그인</Text>
-            </TouchableOpacity>
+            </LoginButton>
 
-            <TouchableOpacity style={styles.loginButton}>
+            <LoginButton>
                 <Text>이메일 로그인</Text>
-            </TouchableOpacity>
+            </LoginButton>
 
-            <View style={styles.loginHelpContainer}>
-                <TouchableOpacity
-                    style={{width: '50%', alignItems: 'center'}}
+            <LoginHelpContainer>
+                <ExtraInfoButton
                     onPress={() => navigation.navigate('extraInfo')}>
                     <Text>이메일 회원가입</Text>
-                </TouchableOpacity>
+                </ExtraInfoButton>
 
-                <TouchableOpacity style={{width: '50%', alignItems: 'center'}}>
+                <ExtraInfoButton>
                     <Text>ID/PW 찾기</Text>
-                </TouchableOpacity>
-            </View>
-        </View>
-    </View>
+                </ExtraInfoButton>
+            </LoginHelpContainer>
+        </LoginContainer>
+    </Container>
 )
 
 export default Login
