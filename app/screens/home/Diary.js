7eea772f5ddcde1e0bd795d604afc2a637ed6068
songmilee <lsm941010@gmail.com>
2021-03-15 22:09:38 +0900

    네비게이션 바 마크업 기반으로 변경

--- app/screens/home/Diary.js
+++ app/screens/home/Diary.js
@@ -1,27 +1,52 @@
-import {createBottomTabNavigator} from 'react-navigation-tabs'
+import React from 'react';
+import {Image} from 'react-native';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
-import Home from './Home'
-import Search from './Search'
-import Write from './Write'
-import Alaram from './Alaram'
-import MyPage from './MyPage'
+import Home from './Home';
+import Write from './Write';
+import MyPage from './MyPage';
 
-const Diary = createBottomTabNavigator({
+import HomeBtn from './images/home-btn.png';
+import HomeBtnActive from './images/home-btn-active.png';
+import WriteBtn from './images/write-btn.png';
+import WriteBtnActive from './images/write-btn-active.png';
+import mypageBtn from './images/mypage-btn.png';
+import mypageBtnActive from './images/mypage-btn-active.png';
+
+const Diary = createBottomTabNavigator(
+  {
     home: {
-        screen: Home,
-    },
-    search: {
-        screen: Search,
+      screen: Home,
     },
     write: {
-        screen: Write,
-    },
-    alaram: {
-        screen: Alaram,
+      screen: Write,
     },
     mypage: {
-        screen: MyPage,
+      screen: MyPage,
     },
-})
+  },
+  {
+    defaultNavigationOptions: ({navigation}) => ({
+      tabBarIcon: ({focused, horizontal, tintIcon}) => {
+        const {routeName} = navigation.state;
 
-export default Diary
+        let imgSrc;
+        if (routeName === 'home') {
+          imgSrc = focused ? HomeBtnActive : HomeBtn;
+        } else if (routeName === 'write') {
+          imgSrc = focused ? WriteBtnActive : WriteBtn;
+        } else {
+          imgSrc = focused ? mypageBtnActive : mypageBtn;
+        }
+
+        return <Image source={imgSrc} />;
+      },
+    }),
+
+    tabBarOptions: {
+      showLabel: false,
+    },
+  },
+);
+
+export default Diary;
