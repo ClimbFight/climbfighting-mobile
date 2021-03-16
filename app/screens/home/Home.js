d16850a03377edd45a5c00c8fb1c28e38640cfa8
songmilee <lsm941010@gmail.com>
2021-03-16 22:58:08 +0900

    Home 화면 상 WatherLayout 추가

--- app/screens/home/Home.js
+++ app/screens/home/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import {View, Text} from 'react-native'
+import WeatherLayout from './WeatherLayer'
 
 const Home = () => (
     <View>
-        <Text>Home</Text>
+        <WeatherLayout
+            location="경기도 수원시"
+            weatherInfo="맑음 -2°"
+            feedback="등산하기 좋은 날씨네요!"
+        />
     </View>
 )
 
---
+++ app/screens/home/WeatherLayer.js
@@ -0,0 +1,65 @@
+import React from 'react'
+import styled from 'styled-components'
+import {colorTheme, globalFont} from '../../styles/variables'
+
+import Sunny from './images/weather-sunny-icon.png'
+import Cloudy from './images/weather-cloudy-icon.png'
+import Thunder from './images/weather-thunder-icon.png'
+import Windy from './images/weather-windy-icon.png'
+import Foggy from './images/weather-foggy-icon.png'
+import Snowy from './images/weather-snowy-icon.png'
+
+const Container = styled.View`
+    width: 100%;
+`
+
+const WeatherInfoContainer = styled.View`
+    padding-top: 16px;
+    padding-left: 24px;
+    padding-bottom: 16px;
+    flex-direction: row;
+`
+
+const WeatherImg = styled.Image`
+    width: 40px;
+    height: 40px;
+    right: 8px;
+`
+
+const WeatherLocation = styled.Text`
+    ${globalFont.ko_light}
+    font-size: 13px;
+`
+
+const WeatherInfoText = styled.Text`
+    ${globalFont.ko_bold}
+    font-size: 18px;
+    justify-content: center;
+    padding-top: 2px;
+`
+
+const WeatherFeedback = styled.Text`
+    ${globalFont.ko_light}
+    font-size: 14px;
+    padding-left: 32px;
+    padding-top: 10px;
+    padding-bottom: 10px;
+    background-color: ${colorTheme['--white-2']};
+`
+
+function WeatherLayout({location, weatherInfo, feedback, logo = Sunny}) {
+    return (
+        <Container>
+            <WeatherInfoContainer>
+                <WeatherImg source={logo} />
+                <Container>
+                    <WeatherLocation>{location}</WeatherLocation>
+                    <WeatherInfoText>{weatherInfo}</WeatherInfoText>
+                </Container>
+            </WeatherInfoContainer>
+            <WeatherFeedback>{feedback}</WeatherFeedback>
+        </Container>
+    )
+}
+
+export default WeatherLayout
