ec87371159e804e1c38be97063a1930241e08b3e
yujeongJeon <workingnewjeong@gmail.com>
2021-04-15 23:57:13 +0900

    [ClimbFight#15] 💄 유효하지 않은 이메일 폼텍스트, 스타일 추가

--- app/screens/auth/EmailForm.js
+++ app/screens/auth/EmailForm.js
@@ -2,17 +2,38 @@ import React, {useState} from 'react'
 import styled from 'styled-components'
 import Layout from './join/Layout'
 
+/**
+ * 빈 값 : --grey-4
+ * 유효한 값 : --marine
+ * 유효하지 않은 값 : --red
+ */
 const Input = styled.TextInput`
     width: 100%;
-    border-bottom-color: ${({theme}) => theme.colors['--grey-4']};
+    border-bottom-color: ${({theme, ...props}) =>
+        props.value.length > 0
+            ? theme.colors['--grey-4']
+            : theme.colors['--red']};
     border-bottom-width: 1px;
     margin-top: 97px;
+`
+
+const FormText = styled.Text`
+    padding: 8px;
+    width: 100%;
+    ${({theme}) => theme.fonts.ko_light}
+    font-size: 12px;
+    color: ${({theme}) => theme.colors['--red']};
 `
 
 const EmailInput = ({navigation}) => {
     const [email, setEmail] = useState('')
 
-    return <Input onChangeText={(value) => setEmail(value)} value={email} />
+    return (
+        <>
+            <Input onChangeText={(value) => setEmail(value)} value={email} />
+            <FormText>유효한 이메일 주소가 아닙니다.</FormText>
+        </>
+    )
 }
 
 const EmailForm = ({navigation}) => {
